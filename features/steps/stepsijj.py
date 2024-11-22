7c666b2dfe18a6ff41dbbcb1840c584011521ae1
AllezitoBR <alle_andrade123@hotmail.com>
2024-11-21 21:29:40 -0300

    automation

---
+++ features/steps/stepsijj.py
@@ -0,0 +1,19 @@
+from behave import given, when, then
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+@given(u'Entro na Página de contato do Instituto Joga Junto')
+def step_impl(context):
+
+
+@when(u'Insiro meus dados')
+def step_impl(context):
+
+
+@when(u'Envio a mensagem "Olá da turma de QA Avançado, Ilhabela Novembro 2024"')
+def step_impl(context):
+
+
+@then(u'Fecho o navegador')
+def step_impl(context):
