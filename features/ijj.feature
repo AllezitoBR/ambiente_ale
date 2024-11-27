2319756a1beef4003a25d1ae164423dbdd8ee36a
AllezitoBR <alle_andrade123@hotmail.com>
2024-11-27 19:34:15 -0300

    testecommit

--- features/ijj.feature
+++ features/ijj.feature
@@ -1,7 +1,8 @@
-Feature: Formulário de Contato
+Feature: Formulario de Contato
 
-  Scenario: Enviar mensagem
-     Given Entro na Página de contato do Instituto Joga Junto
-      When Insiro meus dados
-      And Envio a mensagem "Olá da turma de QA Avançado, Ilhabela Novembro 2024"
-      Then Fecho o navegador
+  Scenario: Enviar Mensagem
+    Given Entro na Página de contato do Instituto Joga Junto
+     When Insiro meus dados
+      When "Envio a mensagem "Teste - Oi!"
+       When Clico no botão enviar
+      
--- features/steps/stepsijj.py
+++ features/steps/stepsijj.py
@@ -5,15 +5,24 @@ from selenium.webdriver.common.keys import Keys
 
 @given(u'Entro na Página de contato do Instituto Joga Junto')
 def step_impl(context):
+    context.driver = webdriver.Firefox()
+    context.driver.get("https://www.jogajuntoinstituto.org/#Contato")
 
 
 @when(u'Insiro meus dados')
 def step_impl(context):
+    context.driver.find_element(By.ID , "nome").send_keys("Alexandre")
+    context.driver.find_element(By.ID , "email").send_keys("ale@teste.com")
+    select = context.driver.find_element(By.ID, "assunto") 
+    for _ in range(7): 
+        select.send_keys(Keys.ARROW_DOWN)
+    select.send_keys(Keys.TAB)
 
-
-@when(u'Envio a mensagem "Olá da turma de QA Avançado, Ilhabela Novembro 2024"')
+@when(u'"Envio a mensagem "Teste - Oi!"')
 def step_impl(context):
+    context.driver.find_element(By.ID, "mensagem").send_keys("Teste - Oi!")
 
-
-@then(u'Fecho o navegador')
-def step_impl(context):
+@when('Clico no botão enviar"{button_id}"') 
+def step_impl(context, button_id): 
+    wait = WebDriverWait(context.driver, 10) 
+    enviar_button = wait.until(EC.element_to_be_clickable((By.ID, button_id)))
