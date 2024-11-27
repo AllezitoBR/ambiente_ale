1eb4b553d23a8070f606acc0a70f6896a24303f2
AllezitoBR <alle_andrade123@hotmail.com>
2024-11-27 19:53:05 -0300

    second commit

--- features/steps/stepsijj.py
+++ features/steps/stepsijj.py
@@ -2,6 +2,8 @@ from behave import given, when, then
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @given(u'Entro na Página de contato do Instituto Joga Junto')
 def step_impl(context):
@@ -22,7 +24,7 @@ def step_impl(context):
 def step_impl(context):
     context.driver.find_element(By.ID, "mensagem").send_keys("Teste - Oi!")
 
-@when('Clico no botão enviar"{button_id}"') 
-def step_impl(context, button_id): 
-    wait = WebDriverWait(context.driver, 10) 
-    enviar_button = wait.until(EC.element_to_be_clickable((By.ID, button_id)))
+    botao_enviar = WebDriverWait(context.driver, 6).until(
+        EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/section[9]/div[1]/form/button"))
+    )
+    context.driver.execute_script("arguments[0].click();", botao_enviar)
